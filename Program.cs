78bd6e9fb74897d8b3979512b0b0ff5f0df90d9b
Thyago Volpatto <tvolpatto@gmail.com>
2026-09-25 08:19:52 -0600

    Class4.2 - created a function for the titles and also add some documentation for the functions

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso"};  
 
+
+/*
+    Displays the logo of the application in ASCII art format.
+*/
 void ExibirLogo()
 {
     Console.WriteLine(@"
@@ -16,6 +20,9 @@ void ExibirLogo()
     Console.WriteLine(mensagemDeBoasVindas);
 }
 
+/*
+    Displays the main menu options to the user and handles their selection.
+*/
 void ExibirOpcoesDoMenu()
 {
     ExibirLogo();
@@ -23,7 +30,7 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
-    Console.WriteLine("Digite -1 para sair");
+    Console.WriteLine("Digite 0 para sair");
 
     Console.Write("\nDigite a sua opção: ");
     string opcaoEscolhida = Console.ReadLine()!;
@@ -39,19 +46,19 @@ void ExibirOpcoesDoMenu()
             break;
         case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
             break;
-        case -1: Console.WriteLine("Tchau tchau :)");
+        case 0: Console.WriteLine("Tchau tchau :)");
             break;
         default: Console.WriteLine("Opção inválida");
             break;
     }
 }
 
+/*
+    Registers a new band by prompting the user for its name and adding it to the list of bands.
+*/
 void RegistrarBanda()
-{
-    Console.Clear();
-    Console.WriteLine("**********************");
-    Console.WriteLine("Registro de bandas");
-    Console.WriteLine("**********************\n");
+{   
+    ExibirTituloDaOpcao("Registro de bandas");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
     listaDasBandas.Add(nomeDaBanda);
@@ -61,12 +68,13 @@ void RegistrarBanda()
     ExibirOpcoesDoMenu();
 }
 
+/*
+    Displays all registered bands to the user.
+*/
 void MostrarBandasRegistradas()
 {
-    Console.Clear();
-    Console.WriteLine("************************************");
-    Console.WriteLine("Exibindo todas as bandas registradas");
-    Console.WriteLine("************************************\n");
+
+    ExibirTituloDaOpcao("Exibindo todas as bandas registradas");
     
     //for (int i = 0; i < listaDasBandas.Count; i++)
     //{
@@ -83,6 +91,19 @@ void MostrarBandasRegistradas()
     Console.Clear();
     ExibirOpcoesDoMenu();
     
+}
+/*
+    Displays a title for the current option, surrounded by asterisks for emphasis.
+    string titulo: The title to display.
+*/
+void ExibirTituloDaOpcao(string titulo)
+{
+    Console.Clear();
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
 }
 
 ExibirOpcoesDoMenu();
